e7b13c138cdfa55ebca1a7e8f379a21f7d38fa15
wangxiang <1827945911@qq.com>
2022-09-27 21:45:58 +0800

    🛺版本更新

--- entfrm-ui/vue.config.js
+++ entfrm-ui/vue.config.js
@@ -19,7 +19,7 @@ module.exports = {
   // 例如 https://www.entfrm.com/。如果应用被部署在一个子路径上，你就需要用这个选项指定这个子路径。例如，如果你的应用被部署在 https://www.entfrm.com/admin/，则设置 baseUrl 为 /admin/。
   publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
   // 在npm run build 或 yarn build 时 ，生成文件的目录名称（要和baseUrl的生产环境路径一致）（默认dist）
-  outputDir: 'dist',
+  outputDir: 'docker/dist',
   // 用于放置生成的静态资源 (js、css、img、fonts) 的；（项目打包之后，静态资源会放在这个文件夹下）
   assetsDir: 'static',
   // 是否开启eslint保存检测，有效值：ture | false | 'error'
